bb8042e84b41d510e0e267e3684d0afe57af045f
khanh <khanhchallenger2086@gmail.com>
2022-05-03 14:21:26 +0700

    create model migration and seeder

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,16 @@
+import { json } from 'express/lib/response';
+import db from '../models/index';
 
-let getHomePage = (req, res) => {
-    return res.render('homepage.ejs');
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll();
+        console.log(data);
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data),
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 let getAboutPage = (req, res) => {
--- src/seeders/seeder-user.js
+++ src/seeders/seeder-user.js
@@ -3,9 +3,15 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Users', [{
-      firstName: 'John',
-      lastName: 'Doe',
-      email: 'example@example.com',
+      email: 'admin@example.com',
+      password: '123456',
+      firstName: 'Hoidanit',
+      lastName: 'khanh',
+      address: 'USA',
+      phonenumber: '123456789',
+      gender: 1,
+      typeRole: 'Role',
+      keyRole: 'R1',
       createdAt: new Date(),
       updatedAt: new Date()
     }]);
